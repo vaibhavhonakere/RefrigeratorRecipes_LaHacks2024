9ce91e546ee14498d18603a8bb42262d94716351
vaibhavhonakere <vhonaker@ucsc.edu>
2024-04-20 13:02:10 -0700

    Finished login team

--- LaHacks/App.js
+++ LaHacks/App.js
@@ -16,7 +16,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='WelcomeScreen' screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name='Home'
           component={Home}
